e490fa07e703c0a10f73ee2fbbcaaf682aeb4727
Philip <philip.balbas@gmail.com>
2018-04-26 09:01:02 +0800

    Finish task

--- report.jsx
+++ report.jsx
@@ -4,8 +4,69 @@ var createReactClass = require('create-react-class')
 
 var rows = require('./data.json')
 
+const reduce = (row, memo) => {
+  if (row.type === 'impression') {
+    memo.impressions = (memo.impressions || 0) + 1
+    return memo
+  }
+  if (row.type === 'load') {
+    memo.loads = (memo.loads || 0) + 1
+    return memo
+  }
+  if (row.type === 'display') {
+    memo.displays = (memo.displays || 0) + 1
+    return memo
+  }
+}
+
+const calculations = [
+  {
+    title: 'Impressions',
+    value: 'impressions',
+    template: (val, row) => val
+  },
+  {
+    title: 'Loads',
+    value: 'loads',
+    template: (val, row) => val
+  },
+  {
+    title: 'Displays',
+    value: 'displays',
+    template: (val, row) => val
+  },
+  {
+    title: 'Load Rate',
+    value: 'loadRate',
+    template: (val, row) => {
+      return `${(row.loads / row.impressions * 100).toFixed(1)}%`
+    }
+  },
+  {
+    title: 'Display Rate',
+    value: 'displayRate',
+    template: (val, row) => {
+      return `${(row.displays / row.loads * 100).toFixed(1)}%`
+    }
+  }
+]
+
 module.exports = createReactClass({
-  render () {
-    return <div>Report</div>
+  render() {
+    return (
+      <div>
+        Report
+        <ReactPivot
+          rows={rows}
+          dimensions={[
+            { value: 'date', title: 'Date' },
+            { value: 'host', title: 'Host' }
+          ]}
+          reduce={reduce}
+          calculations={calculations}
+          activeDimensions={['Date', 'Host']}
+        />
+      </div>
+    )
   }
 })
